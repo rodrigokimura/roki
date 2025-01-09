f57c46a2b8a2c613aeffd4e519ecdf6bbf9351a7
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2025-01-09 01:52:43 -0300

    fix(firmware): avoid initialization of used pin

--- roki/firmware/code.py
+++ roki/firmware/code.py
@@ -11,8 +11,7 @@ async def main():
     rows = ("P0_24", "P1_00", "P0_11", "P1_04", "P1_06")
     cols = ("P0_09", "P0_10", "P1_11", "P1_13", "P1_15", "P0_02")
     encoder_pins = ("P0_17", "P0_20")
-    # thumb_stick_pins = ("P0_31", "P0_29")
-    thumb_stick_pins = ("P_022", "AIN7", "AIN5")
+    thumb_stick_pins = ("P0_22", "AIN7", "AIN5")
 
     roki = Roki.build(rows, cols, thumb_stick_pins, encoder_pins)
     await roki.run()
